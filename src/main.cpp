15429e244afcb054dc2fb2c865926b0c84f2ec8f
Timothy Gibbons <tjgibbons10@gmail.com>
2020-03-07 17:17:24 -0600

    Multithreading

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,18 @@ static std::string port = "/dev/cu.usbmodem01";
 static UNL::UAV::Serial serial(port, 57600);
 static bool running = true;
 static pthread_t readThreadID;
+static pthread_t writeThreadID;
 
 void quit_handler( int sig ){
 	std::cout << "Received " << sig << std::endl;
 	running = false;
+	::pthread_join(readThreadID, NULL);
+	::pthread_join(writeThreadID, NULL);
 	serial.stop();
 }
 
 //TODO: Dedicated Thread...
-void readRunner(){
-	std::cout << "Started Reader";
+void* readRunner(void* arg){
 	uint8_t hasDecoded =0;
 	mavlink_message_t msg;
 	mavlink_status_t status;
@@ -26,7 +28,6 @@ void readRunner(){
 		uint8_t cp;
 		int res = serial.read(cp);
 		if(res){
-			std::cout << "read: " << cp;
 			hasDecoded = mavlink_parse_char(MAVLINK_COMM_1, cp, &msg, &status);
 			if(hasDecoded){
 				std::cout << std::endl;
@@ -35,6 +36,7 @@ void readRunner(){
 			}
 		}
 	}
+	::pthread_exit(NULL);
 }
 
 void mavWriter(mavlink_message_t* message){
@@ -44,13 +46,29 @@ void mavWriter(mavlink_message_t* message){
 	std::cout << "Written " << leng << std::endl;
 }
 
+void writeHeartbeat(){
+	mavlink_message_t heartbeat;             //message holder
+	mavlink_msg_heartbeat_pack(1, 255, &heartbeat, 2, 0, 128, 0, 7);
+	mavWriter(&heartbeat);
+	std::cout << "Sent Heartbeat" << std::endl;
+}
+
+void* writeRunner(void* test){
+	writeHeartbeat();
+	::pthread_exit(NULL);
+}
+
 int main(int argc, char** argv){
 	signal(SIGINT,quit_handler);
 	serial.start();
 	std::cout << "Started Port" << std::endl;
-	//readRunner();
-    mavlink_message_t heartbeat;             //message holder
-	mavlink_msg_heartbeat_pack(1, 255, &heartbeat, 2, 0, 128, 0, 7);
-	mavWrite(&heartbeat);
+
+	std::cout << "Starting Reader Thread";
+	::pthread_create(&readThreadID, NULL, &readRunner, NULL);
+
+	std::cout << "Writing Started Thread";
+	::pthread_create(&writeThreadID, NULL, &writeRunner, NULL);
+
+	while (running);
 	return 0;
 }
