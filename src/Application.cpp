7143ca17b3fe09230d80a520d00f70c396876148
Timothy Gibbons <tjgibbons10@gmail.com>
2020-03-16 00:23:23 -0500

    Added Heartbeat Protocal

--- src/Application.cpp
+++ src/Application.cpp
@@ -8,13 +8,20 @@ Application::Application(const Serial& serial): _serial(serial){
 
 void Application::init(){
 	this->_serial.start();
+	
 	UNL::UAV::Log::getLogger()->info("Starting Read Thread");
 	::pthread_create(&(this->_readThreadID), NULL, [](void* self) -> void* {
 		return static_cast<Application*>(self)->readThread();
 	}, this);
+	
 	UNL::UAV::Log::getLogger()->info("Starting Write Thread");
 	::pthread_create(&(this->_writeThreadID), NULL, [](void* self) -> void* {
 		return static_cast<Application*>(self)->writeThread();
+	}, this);
+	
+	UNL::UAV::Log::getLogger()->info("Starting Heartbeat Thread");
+	::pthread_create(&(this->_heartbeatThreadID), NULL, [](void* self) -> void* {
+		return static_cast<Application*>(self)->heartbeatThread();
 	}, this);
 }
 
@@ -46,15 +53,37 @@ void* Application::writeThread(){
 	::pthread_exit(NULL);
 }
 
+void* Application::heartbeatThread(){
+	using namespace std::literals::chrono_literals;
+	auto last = std::chrono::high_resolution_clock::now();
+	while(_running){
+		auto now = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<float> time = last - now;
+		if(time.count() > 1.0f){
+			mavlink_message_t message;
+			mavlink_heartbeat_t heartbeat;
+
+			mavlink_msg_heartbeat_encode(0xFF, 0xBE, &message, &heartbeat);
+			
+			//SEND
+			this->writeMavlink(&message);
+
+			last = std::chrono::high_resolution_clock::now();
+		}
+	}
+	::pthread_exit(NULL);
+}
+
 void Application::quitHandler(int sig){
 	UNL::UAV::Log::getLogger()->info(std::string("Received ")+std::to_string(sig));
 	this->_running = false;
-	::pthread_join(_readThreadID, nullptr);
-	::pthread_join(_writeThreadID, nullptr);
+	::pthread_join(this->_readThreadID, nullptr);
+	::pthread_join(this->_writeThreadID, nullptr);
+	::pthread_join(this->_heartbeatThreadID, nullptr);
 	_serial.stop();
 }
 
-void Application::mavWriter(mavlink_message_t* message){
+void Application::writeMavlink(mavlink_message_t* message){
 	char buffer[300];
 	unsigned len = mavlink_msg_to_send_buffer((uint8_t*)buffer, message);
 	int leng = _serial.write(buffer, len);
@@ -62,6 +91,6 @@ void Application::mavWriter(mavlink_message_t* message){
 }
 
 Application::~Application(){
-	this->quitHandler(-1);
+	this->quitHandler(0);
 }
 };
